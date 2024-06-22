930b5a3df0dd75c0df6876472d64ec3631a83919
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-06-22 17:15:23 -0400

    fixes redirect

--- routes/users.js
+++ routes/users.js
@@ -34,15 +34,17 @@ router.get("/login", (req, res) => {
 
 router.post(
   "/login",
+  // use the storeReturnTo middleware to save the returnTo value from session to res.locals
   storeReturnTo,
+  // passport.authenticate logs the user in and clears req.session
   passport.authenticate("local", {
     failureFlash: true,
     failureRedirect: "/login",
   }),
+  // Now we can use res.locals.returnTo to redirect the user after login
   (req, res) => {
     req.flash("success", "Welcome back!");
-    const redirectUrl = req.locals.returnTo || "/campgrounds";
-    delete req.session.returnTo;
+    const redirectUrl = res.locals.returnTo || "/campgrounds"; // update this line to use res.locals.returnTo now
     res.redirect(redirectUrl);
   }
 );
